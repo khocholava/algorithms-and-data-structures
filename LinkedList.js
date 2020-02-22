951719ec55883c3995797737426338730df4cb08
khocholava <akokhocholava@gmail.com>
2020-02-22 12:35:49 -0800

    Implemented some data structures 2

---
+++ LinkedList.js
@@ -0,0 +1,108 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let node = new Node(val);
+        if (!this.head) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
+    }
+
+    //Remove node from the end of the linked list
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = newTail;
+        }
+        return current;
+    }
+
+    shift() {
+        if (!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        return currentHead;
+    }
+
+    unshift(element) {
+        const node = new Node(element);
+        if (!this.head) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let counter = 0;
+        let current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, val){
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+
+        let newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+}
+
+let list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.insert(1, 15)
+console.log(list);
